9932476185bf28230ca0d7f39d9393864c1c1241
Heidi Wikman <heidi.wikman@kapsi.fi>
2021-01-31 21:14:50 +0200

    Added attached files

--- ApplicationEmailer/ApplicationEmailer.cs
+++ ApplicationEmailer/ApplicationEmailer.cs
@@ -1,37 +1,71 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using System.Text.Json;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.Storage;
 
 namespace Hakemus
 {
     public static class ApplicationEmailer
     {
         [FunctionName("ApplicationEmailer")]
-        public static void Run([QueueTrigger("outqueue", Connection = "AzureWebJobsStorage")] string messageItem,
+        public static void Run([QueueTrigger("emailqueue", Connection = "AzureWebJobsStorage")] string messageItem,
             ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {messageItem}");
             dynamic json = JsonConvert.DeserializeObject(messageItem);
 
-            Console.WriteLine(json.subject);
             string subject = json.subject;
-
-            Console.WriteLine(json.content);
             string content = json.content;
-
-            Console.WriteLine(json.recipient);
             string recipient = json.recipient;
 
-            Execute(subject, content, recipient).Wait();
+            var filestorageConnection = Environment.GetEnvironmentVariable("FileStorage");
+            var storageAccount = CloudStorageAccount.Parse(filestorageConnection);
+
+            string containerName = "files";
+
+            string CVFileName = "CV.pdf";
+            string applicationFileName = "Hakemus.pdf";
+
+            string[] attachmentCV = { GetAttachment(containerName, CVFileName), CVFileName };
+            string[] attachmentApplication = { GetAttachment(containerName, applicationFileName), applicationFileName };
+
+            string[][] attachments = { attachmentCV, attachmentApplication };
+
+            Execute(subject, content, recipient, attachments).Wait();
         }
 
-        static async Task Execute(String subject, String content, String recipient)
+        
+        
+        public static string GetAttachment(string containerName, string fileName)
+        {
+            var FileStorageConnection = Environment.GetEnvironmentVariable("FileStorage");
+            var storageAccount =  CloudStorageAccount.Parse(FileStorageConnection);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+
+            CloudBlockBlob blob = container.GetBlockBlobReference(fileName);
+
+            string b64String;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                blob.DownloadToStream(memoryStream);
+                var bytes = memoryStream.ToArray();
+                b64String = Convert.ToBase64String(bytes);
+            }
+
+            return b64String;
+        }
+
+
+
+        static async Task Execute(String subject, String content, String recipient, string[][] attachments)
         {
 
             try
@@ -47,8 +81,18 @@ namespace Hakemus
                 };
                 msg.AddTo(new EmailAddress(recipient, "Heidi"));
 
+                foreach (string[] i in attachments)
+                {
+                    Attachment attachment = new Attachment();
+                    attachment.Content = i[0];
+                    attachment.Type = "application/pdf";
+                    attachment.Filename = i[1];
+                    attachment.Disposition = "attachment";
+                    msg.AddAttachment(attachment);
+                }
+
                 var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
-                Console.WriteLine("Status code: " + response.StatusCode);
+                Console.WriteLine("SendGrid status code: " + response.StatusCode);
 
             }
             catch (Exception ex)
